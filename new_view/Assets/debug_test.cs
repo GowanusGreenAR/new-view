16dfafe32f6750dd51bfb4a30392c3a514be2ece
ukpfilms <61725219+ukpfilms@users.noreply.github.com>
2021-10-22 21:21:12 -0400

    Made New Jack Scene

---
+++ new_view/Assets/debug_test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debug_test : MonoBehaviour
+{
+    float num;
+    // Start is called before the first frame update
+    void Start()
+    {
+        num = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        num += 1;
+        Debug.Log(num);
+    }
+}
