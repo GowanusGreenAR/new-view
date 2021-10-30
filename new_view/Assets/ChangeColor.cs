6f9c01283cd7f8c0b7e11056539409a76330a6d6
Shinnosuke Komiya <>
2021-10-30 15:28:50 -0400

    add Shin's test scene

---
+++ new_view/Assets/ChangeColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraPos = GameObject.Find("[VRTK]_VIVE").transform.position;
+        this.gameObject.transform.position = cameraPos;
+    }
+    // 当たった時に呼ばれる関数
+    void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Renderer>().material.color = new Color32(248, 168, 133, 1);
+    }
+}
